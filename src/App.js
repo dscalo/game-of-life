50daa4e0d6dbc3f2fd485ce2c57531852841673d
savageDan94 <savageDan94@gmail.com>
2021-09-03 23:14:28 -0400

    game logic, clickable canvas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import {useRef, useEffect, useState} from 'react'
 import styled from 'styled-components'
 
+import {getCellState,countLiveNeighbors } from './utils/functions'
+
+const X_LENGTH = 800
+const Y_LENGTH = 600
+
 const Canvas = styled.canvas`
   border: 1px red solid;
 `
@@ -12,31 +17,93 @@ const Container = styled.div`
   align-items: center;
 `
 
-const drawPoint = (ctx, x, y) => {
+const drawPoint = (ctx, y, x) => {  
+  ctx.fillRect(x * 10, y * 10, 10 ,10)
+}
+
+
+const playGame = (grid) => {
+  let newGrid = Array(grid.length).fill(0).map(_ => Array(grid[0].length).fill(0))
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      const neighbors = countLiveNeighbors(grid,y,x)
+      const state = getCellState(grid[y][x], neighbors)
+      newGrid[y][x] = state
+    }
+  }
+
+  return newGrid
+}
+
+const drawGrid = (ctx, grid) => {
+  ctx.clearRect(0,0,X_LENGTH, Y_LENGTH)
   ctx.fillStyle = 'black'
-  ctx.fillRect(x*10, y* 10, 10 ,10)
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === 1) {
+        drawPoint(ctx,y,x)
+      }
+    }
+  }
 }
 
 const App = () => {
 
   const canvasRef = useRef()
-  const [game, setGame] = useState([])
+  const [grid, setGrid] = useState(Array(Y_LENGTH / 10).fill(0).map(_ => Array(X_LENGTH / 10).fill(0)))
+  const [intervalId, setIntervalId] = useState()
 
-  useEffect(() => {
-    const ctx = canvasRef.current.getContext('2d');
+  const handleStart = () => {  
+    let curGrid = grid
+    const id = setInterval(() => {
+      const newGrid = playGame(curGrid)
     
- 
-  }, [])
+      setGrid(newGrid)
+      drawGrid(canvasRef.current.getContext('2d'), newGrid)
+      curGrid = newGrid
 
+    }, 650)
+    setIntervalId(id)
+  }
 
+  const handleStop = () => {
+    if (intervalId) {
+      clearInterval(intervalId)
+      setIntervalId(null)
+      return
+    }    
+  }
 
+  const handleCanvasClick = event => {
+    if(intervalId) {
+      return
+    }
+
+    const rect = canvasRef.current.getBoundingClientRect()  
+    const x = Math.floor((event.clientX - rect.left) / 10)
+    const y = Math.floor((event.clientY - rect.top) / 10)
+      
+    grid[y][x] = grid[y][x] === 0 ? 1 : 0
+    setGrid(grid)
+    drawGrid(canvasRef.current.getContext('2d'),grid)    
+  }
 
   return (
   <Container>
-    <h1>Hey there</h1>
-    <Canvas ref={canvasRef} width="800" height="600">
-
+    <h1>Conway's Game of Life</h1>
+    <Canvas 
+      onClick={handleCanvasClick} 
+      ref={canvasRef} 
+      width={`${X_LENGTH}`} 
+      height={`${Y_LENGTH}`}>
+        Browser not supported!
     </Canvas>
+    <div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </div>
   </Container>)
 }
 
